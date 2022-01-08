e0d8fc53b14d420a6f6271c88ba0eccd4f66bfba
Max Yanchenko <maksym.yanchenko93@gmail.com>
2022-01-08 17:38:02 +0300

    upload project progress_group

---
+++ sendmail.php
@@ -0,0 +1,74 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('eng', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('$name');
+	//Кому отправить
+	$mail->addAddress('maksym.yanchenko93@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Informmation';
+
+	//Как с Вами связаться?:
+	$choice = "Texting";
+	if($_POST['choice'] == "Calling"){
+		$choice = "Calling";
+	}
+
+	//Тело письма
+	$body = '<h1>You selected this project!</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['phone']))){
+		$body.='<p><strong>Phone:</strong> '.$_POST['phone'].'</p>';
+	}
+	if(trim(!empty($_POST['choice']))){
+		$body.='<p><strong>How to contact to you?:</strong> '.$choice.'</p>';
+	}
+	if(trim(!empty($_POST['example']))){
+		$body.='<p><strong>Select your project:</strong> '.$_POST['example'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Message:</strong> '.$_POST['message'].'</p>';
+	}
+	
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Photo</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'error';
+	} else {
+		$message = 'Data has sent!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
